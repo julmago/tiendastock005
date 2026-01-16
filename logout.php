da5f53e3beae6cdc124f63b080561076faaacdf2
julmago <47571542+julmago@users.noreply.github.com>
2026-01-16 16:25:17 -0300

    Fix shared session cookie path

--- logout.php
+++ logout.php
@@ -1,4 +1,21 @@
 <?php
+$cookieParams = session_get_cookie_params();
+session_set_cookie_params([
+  'lifetime' => $cookieParams['lifetime'],
+  'path' => '/',
+  'domain' => $cookieParams['domain'],
+  'secure' => $cookieParams['secure'],
+  'httponly' => $cookieParams['httponly'],
+  'samesite' => $cookieParams['samesite'] ?? 'Lax',
+]);
 session_start();
 session_destroy();
+setcookie(session_name(), '', [
+  'expires' => time() - 3600,
+  'path' => '/',
+  'domain' => $cookieParams['domain'],
+  'secure' => $cookieParams['secure'],
+  'httponly' => $cookieParams['httponly'],
+  'samesite' => $cookieParams['samesite'] ?? 'Lax',
+]);
 header('Location: /');
